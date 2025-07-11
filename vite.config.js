616298efb6019b3ae71219c8e109b8a3f438e304
cotton <129889995+cottonable@users.noreply.github.com>
2025-07-11 16:13:52 +0200

    REDESIGN V4

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// this gives you ide completion for the config obj
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
-});
